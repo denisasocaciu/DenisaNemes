c655a4c313c5094bee6fd081b8d67e056c017c80
Denisa <denisasocaciu59@yahoo.com>
2021-09-30 00:55:49 +0300

    Tema Session 06 - metoda calculatePrice

---
+++ src/ro/sci/s06/calc.java
@@ -0,0 +1,20 @@
+package ro.sci.s06;
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the price without VAT");
+        double priceInit = scanner.nextDouble();
+
+        System.out.println("Enter the VAT (%)");
+        double vat = scanner.nextDouble();
+
+        CalculatePrice(priceInit, vat);
+    }
+    public static void CalculatePrice(double priceInit, double vat) {
+        double a = priceInit + ((19 * priceInit) / 100);
+        System.out.println("THe price with VAT is " + a);
+    }
+}
